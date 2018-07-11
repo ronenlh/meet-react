60f33c56f2fc8b4c25dc5f700a17c5b89fccfa57
Naffar, Nabil (nn605g) <nn605g@intl.att.com>
2018-07-11 18:52:15 +0300

    final-game

--- final-app/memorize/src/components/game-board/game-board.tsx
+++ final-app/memorize/src/components/game-board/game-board.tsx
@@ -3,9 +3,11 @@ import * as React from 'react';
 import { Deck } from '../deck/deck';
 import { CARDS } from '../../models/card-types';
 import './game-board.css';
+// import { Congrats } from '../congrats/congrats';
 
 export const GameBoard = () => (
     <div className="container">
         <Deck cards={CARDS} />
+        {/* <Congrats /> */}
     </div>
 );
